896b31563ce095ed7385af9a2b85003fc479d17c
Matthew Vogee <mvogee@yahoo.com>
2020-11-04 21:23:59 -0800

    basic starter code from GLFW working

---
+++ first-try/open_window.cpp
@@ -0,0 +1,41 @@
+#include <GLFW/glfw3.h>
+
+
+
+// clang++ -std=c++11 -stdlib=libc++ -Wno-deprecated-declarations -lglfw -framework CoreVideo -framework OpenGL -framework IOKit -framework Cocoa -framework Carbon open_window.cpp
+
+int main(void)
+{
+    GLFWwindow* window;
+
+    /* Initialize the library */
+    if (!glfwInit())
+        return -1;
+
+    /* Create a windowed mode window and its OpenGL context */
+    window = glfwCreateWindow(640, 480, "Hello World", NULL, NULL);
+    if (!window)
+    {
+        glfwTerminate();
+        return -1;
+    }
+
+    /* Make the window's context current */
+    glfwMakeContextCurrent(window);
+
+    /* Loop until the user closes the window */
+    while (!glfwWindowShouldClose(window))
+    {
+        /* Render here */
+        glClear(GL_COLOR_BUFFER_BIT);
+
+        /* Swap front and back buffers */
+        glfwSwapBuffers(window);
+
+        /* Poll for and process events */
+        glfwPollEvents();
+    }
+
+    glfwTerminate();
+    return 0;
+}
